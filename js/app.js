cbd6769ffd8f1b1806a8e5ac41a5809e61c25700
JessicaBarnett <BarnettEffect@gmail.com>
2014-12-13 23:48:45 -0500

    Have  loading JSON in but the directives don't refresh once the data is available.

--- js/app.js
+++ js/app.js
@@ -13,132 +13,40 @@ function setURL() {
 
 setURL();
 
-
-
 //Module
 var blogApp = angular.module('blogApp', []);
 
-//factory to generate blog post data
-blogApp.factory('BlogData', function($http){
-	var blogData = {};
 
-	// blogData.externalPosts = $http('get', [{'url': 'blogData.json'}]);
-	// blogData.externalPosts.success(function(response) {
-	// 	console.log("totally worked!");
-	// 	console.dir(response);
- //    	// return response.data;
-	// });
-	// blogData.externalPosts.failure =function(response){
-	// 	console.log("failed to load omg noooo!!");
-	// 	console.log(response);
-	// };
-	// console.log(blogData.externalPosts);
+//service to load blog data
+blogApp.service('BlogDataModel', function($http, $q){
+	var blogDataModel = this, //$this
+		URLS = {
+			FETCH: 'blogData.json'
+		};
 
-	blogData.posts = [{
-		"title": "It's Cheesey, but It'll Make You Feel Grate!",
-		"author": "Emmental",
-		"date": new Date(2014, 09, 23),
-		"tags": ["Kitchen Utensils", "Cheese"],
-		"body": "\tFondue manchego stilton. Babybel roquefort the big cheese queso croque monsieur rubber cheese pecorino feta. Gouda queso pecorino cheese and biscuits cottage cheese who moved my cheese danish fontina emmental. Edam goat paneer boursin cheesy grin taleggio cauliflower cheese stinking bishop. Gouda.\n\tThe big cheese chalk and cheese everyone loves. Dolcelatte cottage cheese babybel cheese slices macaroni cheese cheddar everyone loves airedale. Bocconcini cow red leicester fromage cheese on toast cauliflower cheese halloumi emmental. Everyone loves the big cheese.\n\tCroque monsieur taleggio cheese strings. Cheesy feet cheesecake parmesan caerphilly cheesy grin say cheese cheese triangles rubber cheese. Feta fondue fromage swiss mascarpone rubber cheese caerphilly stilton. Mascarpone cauliflower cheese fondue cottage cheese red leicester dolcelatte jarlsberg babybel. Chalk and cheese st. agur blue cheese.\n\tFeta red leicester mascarpone. Fondue cut the cheese cheese slices cheese strings ricotta say cheese mascarpone cheesecake. Hard cheese stinking bishop mozzarella emmental mozzarella cheese and wine camembert de normandie who moved my cheese. Smelly cheese goat stilton cream cheese.\n\tDanish fontina ricotta port-salut. Ricotta cheeseburger smelly cheese swiss cheesy grin fromage parmesan red leicester. Pecorino say cheese danish fontina pecorino mascarpone fromage frais cheese slices cow. Port-salut cheese and wine pecorino say cheese fondue danish fontina when the cheese comes out everybody's happy paneer. Bavarian bergkase cheese and wine everyone loves cheese slices.",
-		"comments": [{
-			"author": "Ricotta",
-			"email": "cheese@please.com",
-			"website": "https://www.google.com/search?q=ricotta",
-			"body": "I love Cheese Jokes!!",
-			"image": "../images/cheese_icon.svg",
-			"date": new Date(2014, 10, 12)
-		}, 
-		{
-			"author": "Limburgher",
-			"email": "cheese@please.com",
-			"website": "https://www.google.com/search?q=limburgher",
-			"body": "This article is stupid.",
-			"image": "../images/cheese_icon.svg",
-			"date": new Date(2014, 10, 12)
-		},
-		{
-			"author": "Swiss",
-			"email": "cheese@please.com",
-			"website": "https://www.google.com/search?q=swiss",
-			"body": "Stop Gratin' Limburgher!  You stink!",
-			"image": "../images/cheese_icon.svg",
-			"date": new Date(2014, 10, 12)
-		}]
-	},{
-		"title": "Everything's Gouda, but it Could Always be Cheddar!",
-		"author": "Gouda",
-		"date": new Date(2014, 10, 01),
-		"tags": ["Gouda", "Cheddar", "Cheese"],
-		"body": "\tCauliflower cheese when the cheese comes out everybody's happy feta. Cheesecake fromage frais caerphilly monterey jack port-salut camembert de normandie danish fontina babybel. Halloumi swiss cheese and wine emmental stilton smelly cheese chalk and cheese gouda. Halloumi swiss cheeseburger the big cheese.\n\tMonterey jack ricotta halloumi. Parmesan danish fontina gouda emmental say cheese smelly cheese cheese strings goat. Cow croque monsieur jarlsberg macaroni cheese airedale swiss cheese slices croque monsieur. Boursin monterey jack camembert de normandie paneer the big cheese.\n\tSay cheese caerphilly squirty cheese. Stinking bishop say cheese cheesecake cheesy grin the big cheese cheesy feet fromage frais caerphilly. Chalk and cheese who moved my cheese cheese on toast fondue pecorino cheese triangles say cheese when the cheese comes out everybody's happy. Danish fontina parmesan pecorino taleggio danish fontina jarlsberg edam paneer. Bocconcini taleggio.\n\tGoat blue castello port-salut. Everyone loves goat cheese and biscuits cheese slices macaroni cheese roquefort goat goat. Danish fontina chalk and cheese cheese strings melted cheese pepper jack blue castello queso gouda. Cheesecake port-salut cheese and wine pecorino stinking bishop squirty cheese caerphilly who moved my cheese. Pepper jack mascarpone cheese on toast hard cheese.\n\tHard cheese bocconcini cottage cheese. Mascarpone fondue taleggio dolcelatte cheeseburger cow cheese and wine bocconcini. Edam cow taleggio cheese and biscuits port-salut mozzarella macaroni cheese edam. Hard cheese cheese on toast cheesy grin.",
-		"comments": 
-		[{
-			"author": "Mozzarella",
-			"email": "cheese@please.com",
-			"website": "https://www.google.com/search?q=mozzarella",
-			"body": "Oh Que... So?",
-			"image": "../images/cheese_icon.svg",
-			"date": new Date(2014, 10, 12)
-		}, 
-		{
-			"author": "Pepper Jack",
-			"email": "cheese@please.com",
-			"website": "https://www.google.com/search?q=pepper%20jack",
-			"body": "This Blog Roqueforts!",
-			"image": "../images/cheese_icon.svg",
-			"date": new Date(2014, 10, 12)
-		},
-		{
-			"author": "Montery Jack",
-			"email": "cheese@please.com",
-			"website": "https://www.google.com/search?q=montery%20jack",
-			"body": "Halloumi to bid you fondue.",
-			"image": "../images/cheese_icon.svg",
-			"date": new Date(2014, 10, 12)
-		}]
-	},{
-		"title": "The Last Slice: Forever Provolone",
-		"author": "Brie",
-		"date": new Date(2014, 10, 12),
-		"tags": ["Provolone", "Cheese"],
-		"body": "\tRoquefort smelly cheese fromage. Babybel cut the cheese fromage cottage cheese bocconcini edam hard cheese say cheese. Stilton emmental dolcelatte cheese and biscuits jarlsberg gouda port-salut pepper jack. Brie cheesy grin feta lancashire cheesy grin edam taleggio cow. Cheese strings.\n\tCheesecake paneer ricotta. Camembert de normandie jarlsberg manchego cheesecake queso lancashire roquefort fromage frais. Parmesan chalk and cheese cheesy feet smelly cheese monterey jack smelly cheese caerphilly mascarpone. Cheesecake pecorino port-salut paneer camembert de normandie cottage cheese parmesan.\n\tFromage caerphilly feta. Edam babybel st. agur blue cheese cut the cheese cheese slices edam cheesecake cut the cheese. Feta port-salut fondue cheese triangles chalk and cheese brie pecorino cheese strings. Fromage frais fondue stinking bishop manchego cheese and wine cheesecake.\n\tDanish fontina cauliflower cheese cheese slices. Who moved my cheese cheesy feet cheese and biscuits roquefort bavarian bergkase swiss fondue cheesecake. Cheesy feet manchego blue castello camembert de normandie edam parmesan edam paneer. When the cheese comes out everybody's happy cheese triangles roquefort dolcelatte brie pecorino croque monsieur stilton. Macaroni cheese lancashire stinking bishop chalk and cheese.\n\tSay cheese paneer gouda. Fromage cheese slices cheesecake croque monsieur cheese triangles who moved my cheese parmesan babybel. Pecorino macaroni cheese croque monsieur ricotta dolcelatte roquefort babybel cheese slices. Red leicester rubber cheese cheesy grin croque monsieur.",
-		"comments": 
-		[{
-			"author": "Stilton",
-			"email": "cheese@please.com",
-			"website": "https://www.google.com/search?q=stilton",
-			"body": "Oh, don't be so bleu!  Just always drive caerphilly.",
-			"image": "../images/cheese_icon.svg",
-			"date": new Date(2014, 10, 12)
-		},
-		{
-			"author": "Manchengo",
-			"email": "cheese@please.com",
-			"website": "https://www.google.com/search?q=roquefort",
-			"body": "Don't be a babybel!  Cheese the day!!  Brie all you can brie!!",
-			"image": "../images/cheese_icon.svg",
-			"date": new Date(2014, 11, 12)
-		}, 
-		{
-			"author": "Gruyere",
-			"email": "cheese@please.com",
-			"website": "https://www.google.com/search?q=gruyere",
-			"body": "Boo!  There's a Meunster under your bed!!  Feta run! LOL!!", 
-			"image": "../images/cheese_icon.svg",
-			"date": new Date(2014, 14, 12)
-		},
-		{
-			"author": "Gorgonzola",
-			"email": "cheese@please.com",
-			"website": "https://www.google.com/search?q=gorgonzola",
-			"body": "Whenever I'm paneer Meunsters, I edam up!  Rawr!!",
-			"image": "../images/cheese_icon.svg",
-			"date": new Date(2014, 17, 12)
-		}]
+	blogDataModel.posts;
+	blogDataModel.convertDatesToObjects = function(posts){
+	    //converts all dates in the base json, both at the post and comment level from "mm/dd/yyyy" strings to date objects 
+	    posts.forEach(function(post, index, array){
+	        	post.date = new Date(post.date);
+	        	post.comments.forEach(function(comment, index, array){
+	        		comment.date = new Date(comment.date);
+	        	});
+	       	});
+	    return posts;
+	}
 
-	}];
+	blogDataModel.extractData = function(response){
+		blogDataModel.posts = blogDataModel.convertDatesToObjects(response.data);
+		console.log("in callback");
+	}
 
-	return blogData;
+	blogDataModel.get = function(){
+		//$q sets it up so you're passing a promise to the caller, on which they can call .then, and do their rendering in
+		return (blogDataModel.posts) ? $q.when(blogDataModel.posts) : $http.get(URLS.FETCH).then(blogDataModel.extractData);
+	}
 });
+
 
 //custom date fiter
 blogApp.filter('date', function($filter){
@@ -164,10 +72,16 @@ blogApp.filter('toUrl', function($filter){
 });
 
 //main controller
-blogApp.controller("BlogCtrl", function($scope, BlogData){
-	$scope.blogData = BlogData;
+blogApp.controller("BlogCtrl", function($scope, BlogDataModel){
+	$scope.blogData = BlogDataModel.get().then(function(response){
+		return response.data;
+	});
 });
 
+//*********  Note to self  **********//
+// As of today, 12/13/2014, I'm having problems getting the directives to refresh after the data has been loaded.
+// Using $q to pass my data as a promise now, so a .then can be used in the controller.  but directives still render too early.  
+// abandonning project for the moment.
 
 //title directive
 blogApp.directive("pageTitle", function(){
@@ -193,7 +107,8 @@ blogApp.directive("post", function(){
 		restrict: "E",
 		templateUrl: directory.concat('/templates/post-snippet-template.html'),
 		scope: { //isolate scope
-			post: "="
+			post: "=",
+			blogData: "="
 		},
 		link: function(scope, element, attrs){
 			// console.dir(scope)
@@ -207,7 +122,8 @@ blogApp.directive("fullPost", function(){
 		restrict: "E",
 		templateUrl: directory.concat('/templates/post-page-template.html'),
 		scope: { //isolate scope
-			post: "="
+			post: "=", 
+			blogData: "="
 		},
 		link: function(scope, element, attrs){
 			// console.dir(scope);
@@ -220,8 +136,10 @@ blogApp.directive("fullPost", function(){
 /******* Comments and Comment Form *******/
 
 
-blogApp.controller("commentCtrl", function($scope, BlogData){
-	$scope.blogData = BlogData;
+blogApp.controller("commentCtrl", function($scope, BlogDataModel){
+	$scope.blogData = BlogDataModel.get().then(function(response){
+		return response.data;
+	});
 	$scope.isCommentInProgress = function(){
 		//returns true if there is an author, body, or image in the comment form
 		return ($scope.comment.author || $scope.comment.body || $scope.comment.image) ? true : false;
@@ -274,7 +192,6 @@ blogApp.directive("commentForm", function(){
 
 
 /******* search page *******/
-
 /*
 	//ArchiveItem and Tag formats
 	 
@@ -293,8 +210,10 @@ blogApp.directive("commentForm", function(){
 
 //****** Tag List ******//
 
-blogApp.controller("tagListCtrl", function($scope, BlogData){
-	$scope.blogData = BlogData;
+blogApp.controller("tagListCtrl", function($scope, BlogDataModel){
+	$scope.blogData = BlogDataModel.get().then(function(response){
+		return response.data;
+	});
 	$scope.getTagList = function(blogData){
 		var tagList = [];
 		blogData.posts.forEach(function(post, index, array){
@@ -334,8 +253,10 @@ blogApp.directive("tagList", function(){
 
 //****** Archive List ******//
 
-blogApp.controller("archiveListCtrl", function($scope, $filter, BlogData){
-	$scope.blogData = BlogData;
+blogApp.controller("archiveListCtrl", function($scope, $filter, BlogDataModel){
+	$scope.blogData = BlogDataModel.get().then(function(response){
+		return response.data;
+	});
 	$scope.getArchiveList = function(blogData){
 		var archiveList = []; 
 
